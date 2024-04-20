20cea58a2bd6d3e3c87b90d4a16b38d16b42dd34
DHP003 <93103816+RebelGhost03@users.noreply.github.com>
2024-04-20 14:43:44 +0700

    Fixed visualization bug

--- Map 2/network.js
+++ Map 2/network.js
@@ -45,8 +45,8 @@ class Level{
       this.inputs=new Array(inputCount);
       this.outputs=new Array(outputCount);
       this.biases=new Array(outputCount);
-
       this.weights=[];
+      
       for(let i=0;i<inputCount;i++){
          this.weights[i]=new Array(outputCount);
       }
